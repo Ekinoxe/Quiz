74170092fc6a4519b4e3a42901cfcf20b6893bab
Ekinox <kouassi.duchaine@protonmail.com>
2023-02-05 19:20:41 +0100

    Delete | Modesl/Question/2-props ( Date/created/update )

--- Models/Question.cs
+++ Models/Question.cs
@@ -4,7 +4,5 @@
     {
         public int QuestionID { get; set; }
         public string Text { get; set; } = null!;
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdateAt { get; set; }
     }
 }

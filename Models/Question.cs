d6244fdcc61ecdc47be7e425e270b80349d00387
Ekinox <kouassi.duchaine@protonmail.com>
2023-02-05 19:18:56 +0100

    Update | Modesl/Question/props ( text -> not nullable )

--- Models/Question.cs
+++ Models/Question.cs
@@ -3,7 +3,7 @@
     public class Question
     {
         public int QuestionID { get; set; }
-        public string? Text { get; set; }
+        public string Text { get; set; } = null!;
         public DateTime CreatedAt { get; set; }
         public DateTime UpdateAt { get; set; }
     }

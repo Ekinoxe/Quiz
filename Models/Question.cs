379f90832e12bfeb097220693a500b1249795744
Ekinox <kouassi.duchaine@protonmail.com>
2023-02-05 19:04:30 +0100

    Create | Model/QuizDbContext

--- Models/Question.cs
+++ Models/Question.cs
@@ -3,7 +3,7 @@
     public class Question
     {
         public int QuestionID { get; set; }
-        public string Text { get; set; }
+        public string? Text { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdateAt { get; set; }
     }
---
+++ Models/QuizDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Quiz.Models
+{
+    public partial class QuizDbContext : DbContext
+    {
+        public QuizDbContext()
+        { }
+        public QuizDbContext(DbContextOptions<QuizDbContext> options)
+            : base(options)
+        { }
+        public virtual DbSet<Question> Movies { get; set; }
+    }
+}
